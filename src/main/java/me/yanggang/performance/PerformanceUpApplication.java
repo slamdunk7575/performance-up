a49f58339866526327b78a0443cb4d2c98a5c90a
slamdunk7575 <slamdunk7575@gmail.com>
2024-02-19 17:28:57 +0900

    (feat) init commit

---
+++ src/main/java/me/yanggang/performance/PerformanceUpApplication.java
@@ -0,0 +1,13 @@
+package me.yanggang.performance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformanceUpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PerformanceUpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/yanggang/performance/PerformanceUpApplicationTests.java
@@ -0,0 +1,13 @@
+package me.yanggang.performance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerformanceUpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
